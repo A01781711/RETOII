6a0f4910e97f4129e73748a33381d34d83002412
A01781711 <A01781711@tec.mx>
2024-10-25 13:26:15 -0600

    Add files via upload

---
+++ qgis2web_2024_10_25-13_23_21_888033/data/PREDIO_4.js
@@ -0,0 +1,1 @@
+var json_PREDIO_4 = {"type":"FeatureCollection","name":"PREDIO_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"latitude":19.42487,"longitude":-99.15664,"label":"RETO_PREDIO"},"geometry":{"type":"Point","coordinates":[-99.156641312575374,19.424870246315315]}}]}
